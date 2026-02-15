aa97da33080c43ff04175777443dadc9d70180ac
Sascha Bartl <bartl.sascha0@gmail.com>
2026-02-15 13:02:33 +0100

    Add support for Paper 1.21.11

--- bukkit/src/main/java/com/ruinscraft/panilla/bukkit/PanillaPlugin.java
+++ bukkit/src/main/java/com/ruinscraft/panilla/bukkit/PanillaPlugin.java
@@ -153,6 +153,21 @@ public class PanillaPlugin extends JavaPlugin implements IPanilla {
     private void initVersion() {
         getLogger().info("DATA VERSION " + Bukkit.getUnsafe().getDataVersion());
 
+        // Paper 1.21.11
+        if (Bukkit.getUnsafe().getDataVersion() == 4671) {
+            packetSerializerClass = com.ruinscraft.panilla.paper.v1_21_11.io.dplx.PacketSerializer.class;
+            protocolConstants = new IProtocolConstants() {
+                @Override
+                public int maxBookPages() {
+                    return 100;
+                }
+            };
+            playerInjector = new com.ruinscraft.panilla.paper.v1_21_11.io.PlayerInjector();
+            packetInspector = new com.ruinscraft.panilla.paper.v1_21_11.io.PacketInspector(this);
+            containerCleaner = new com.ruinscraft.panilla.paper.v1_21_11.InventoryCleaner(this);
+            return;
+        }
+
         // Paper 1.21.5
         if (Bukkit.getUnsafe().getDataVersion() == 4440) {
             packetSerializerClass = com.ruinscraft.panilla.paper.v1_21_5.io.dplx.PacketSerializer.class;
